a10f7f9ea63d9900fbe3a3c6b4ff14206aac92d4
pradeepaparandaman <pradeepaparandaman@gmail.com>
2016-01-28 22:36:13 +0530

    Create bitnos.c

---
+++ bitnos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int binaryDigits(int);
+void main()
+{
+	int num,x;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	x=binaryDigits(num);
+	printf("The number of binary digits is %d\n",x);	
+}
+int binaryDigits(int num)
+{
+	int count=0;
+	if(num==0)
+	{
+		return 1;
+	}
+	while(num!=0)
+	{
+		num=num/2;
+		count++;
+	}
+	return count;
+}
+
